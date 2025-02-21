16728e4fa73bebc395df6e798e90e4e062c882c5
GrnRant <gn.rant@gmail.com>
2025-02-21 19:23:37 -0300

    Avances en gen_tiles.

--- VGA/gen_tiles.vhd
+++ VGA/gen_tiles.vhd
@@ -4,25 +4,54 @@ use ieee.numeric_std.all;
 
 entity gen_tiles is
     generic(
-            N_CORDIC : natural := 16;
-            N_ADDRESS : natural := 16;
-            N_DATA : natural := 16;
+            N_CORDIC: natural := 16;
+            N_ADDRESS: natural := 15; --Memoria de 32kx1bit -> address máximo es 32000
+            N_DATA: natural := 16; --Memoria de 32kx1bit -> "words" son de 1bit
             MAX_VAL : natural := 50 --Se calculó en base a BRAM y resolución de 480x640
     );
     port(
+        rst : in std_logic;
+        clk : in std_logic;
         x_in : in signed(N_CORDIC-1 downto 0);
-        y_in : in signed(N_C-1 downto 0);
-        z_in : in signed(N_C-1 downto 0);
+        y_in : in signed(N_CORDIC-1 downto 0);
         cordic_busy : in std_logic;
-        address : out unsigned(N_ADDRESS-1 downto 0);
+        wr : out std_logic;
+        addr : out unsigned(N_ADDRESS-1 downto 0);
         wr_data : out unsigned(N_DATA-1 downto 0)
     );
-end gen_tiles
+end gen_tiles;
 
 architecture gen_tiles_arch of gen_tiles is
-    
+    constant cordic_scale: integer := 2**(N_CORDIC-1)/MAX_VAL;
+    signal x_coord: integer := 0;
+    signal y_coord: integer := 0;
+    signal bit_index: integer := 0;
+    signal busy_pre_state: std_logic := '0';
 begin
-    
-    
-    
+    P_GEN_TILES_MAIN: process(clk)
+    begin
+        if rising_edge(clk) then
+            --Reset
+            if rst = '1' then
+                wr <= '0';
+                addr <= (others => '0');
+                wr_data <= (others => '0');
+            end if;
+            --Si se detectó que bajó la línea de busy, habilitar escritura
+            if (busy_pre_state = '1' and cordic_busy = '0') then
+                wr <= '1';
+            else
+                wr <= '0';
+            end if;
+            busy_pre_state <= cordic_busy;
+        end if;
+    end process;
+
+    --Esto solo escribe un punto, necesito que haga dibujo entero
+    x_coord <= to_integer(x_in)/cordic_scale;
+    y_coord <= to_integer(y_in)/cordic_scale;
+    bit_index <= x_coord - 160*y_coord + 9680;
+    addr <= to_unsigned(bit_index/N_DATA, N_DATA);
+    wr_data <= to_unsigned(bit_index mod N_DATA, N_DATA);
+
 end architecture gen_tiles_arch;
--- build.bat
+++ build.bat
@@ -5,19 +5,26 @@ if exist build\ (
 ) 
 mkdir build
 
+@REM utilities
+ghdl -a --workdir=build utilities/utilities.vhd
+ghdl -a --workdir=build utilities/ffd.vhd
+ghdl -a --workdir=build utilities/counter.vhd
+
 @REM uart_cmd
 ghdl -a --workdir=build uart_cmd/meta_harden.vhd
 ghdl -a --workdir=build uart_cmd/uart_baud_gen.vhd
 ghdl -a --workdir=build uart_cmd/uart_rx_ctl.vhd
 ghdl -a --workdir=build uart_cmd/uart_rx.vhd
-ghdl -a --workdir=build utilities/utilities.vhd
 ghdl -a --workdir=build uart_cmd/cmd_ctl.vhd
+
+@REM cordic
 ghdl -a --workdir=build cordic/cordic_ctl.vhd
-ghdl -a --workdir=build utilities/ffd.vhd
-ghdl -a --workdir=build utilities/counter.vhd
 ghdl -a --workdir=build cordic/cordic_stage.vhd
 ghdl -a --workdir=build cordic/precordic.vhd
 ghdl -a --workdir=build cordic/cordic.vhd
+
+@REM vga
+ghdl -a --workdir=build vga/gen_tiles.vhd
 
 @REM GENERAL
 ghdl -a --workdir=build vector_rotator.vhd
